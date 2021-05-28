adea22d8a3164f9b090cce8e4913423f5651f6b0
mstrivens <max.strivens@live.co.uk>
2021-05-28 14:09:41 +0100

    ensures model check for password presence

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before do
-    @user = User.new(username:  "user1", email: 'user@email.com', password: 'password1')
+  describe 'when fields are filled in' do
+    before do
+      @user = User.new(username:  "user1", email: 'user@email.com', password: 'password1')
+    end
+
+    it 'responds to username' do
+      expect(@user).to respond_to(:username)
+    end
+
+    it 'responds to password' do
+      expect(@user).to respond_to(:password)
+    end
+
+    it 'responds to email' do
+      expect(@user).to respond_to(:email)
+    end
+
+    it 'is valid' do
+      expect(@user).to be_valid
+    end
   end
 
-  it 'responds to username' do
-    expect(@user).to respond_to(:username)
-  end
-
-  it 'responds to password' do
-    expect(@user).to respond_to(:password)
-  end
-
-  it 'responds to email' do
-    expect(@user).to respond_to(:email)
-  end
-
-  it 'is valid' do
-    expect(@user).to be_valid
-  end
-
-  describe "when field is empty" do
+  describe "when username is empty" do
     before do
       @user = User.new(username:  "", email: 'user@email.com', password: 'password1')
+    end
+
+    it 'is not valid' do
+      expect(@user).to_not be_valid
+    end
+  end
+
+  describe "when password is empty" do
+    before do
+      @user = User.new(username:  "user1", email: 'user@email.com', password: '')
     end
 
     it 'is not valid' do
