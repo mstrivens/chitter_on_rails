e8ec688c113aaa76fbca33ad3d108e6c6ebbc9d9
mstrivens <max.strivens@live.co.uk>
2021-05-28 13:53:35 +0100

    Refatored logic in users controller into helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    @user.save
+    create_user_object(user_params)
+    save_user_object
     redirect_to root_url
   end
 
   private
+  def create_user_object(user_params)
+    @user = User.new(user_params)
+  end
+
+  def save_user_object
+    @user.save
+  end
 
   def user_params
     params.require(:user).permit(:username, :password, :email)
