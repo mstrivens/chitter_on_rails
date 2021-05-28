8b75c9612e1a416f547284eb22ab48c13c446324
mstrivens <max.strivens@live.co.uk>
2021-05-28 11:52:34 +0100

    User controller tests passing for adding new user object to database

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe "GET/users/new" do
+    it 'responds with 200' do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+
+  describe "POST/users/create" do
+    let(:create_user) { User.create(username: 'user1', password: 'password1', email: 'user@email.com') }
+    it 'responds with 201' do
+      expect{ create_user }.to change{ User.count }.by(1)
+    end
+  end
+
+    it "creates a user" do
+      post :create, params: { user: { username: "user1", password: "password1", email: 'user@email.com' } }
+      expect(User.find_by(username: "user1")).to be
+    end
+  end
+end
