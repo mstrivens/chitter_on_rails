7bbbbd212a133bb866bae6c3ab968407d2772315
mstrivens <max.strivens@live.co.uk>
2021-06-02 12:38:13 +0100

    Rafactored application controller to use private methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,15 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
+      set_current_user_to(session[:user_id])
     else
       @current_user = nil
     end
+  end
+
+private
+
+  def set_current_user_to(session_id)
+    @current_user ||= User.find(session_id)
   end
 end
